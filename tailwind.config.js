29fa905a714f3ca144d16c2eb05ae494cab51e61
haha861924 <qq861924@gmail.com>
2021-09-05 01:49:01 +0800

    fix tailwind setting

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   purge: ['index.html','./src/**/*.{js,jsx,ts,tsx,vue,html}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        purplest: '#733FC8'
+      }
+    },
   },
   plugins: [],
 }
