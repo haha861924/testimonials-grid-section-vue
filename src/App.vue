aef1a107845da622535f41c87c0c956ec4893775
haha861924 <qq861924@gmail.com>
2021-09-05 01:48:41 +0800

    add card

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,27 @@
 <template>
-  <div class="text-center">
-    <img class="mx-auto mt-12 mb-8" alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Vite + Vue 3 + Tailwind jit" />
-    <img class="mx-auto my-8" alt="powered-by" src="./assets/powered-by-vitawind-bright.png">
+  <div class="text-center bg-purplest">
+    <Card :bgColor="'red-500'" :cardContent="cardContent[0]"></Card>
   </div>
 </template>
 
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-
-// This starter template is using Vue 3 experimental <script setup> SFCs
-// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+<script>
+import Card from './components/Card.vue'
+export default {
+  name: "App",
+  components:{Card},
+    data() {
+        return {
+          cardContent:[
+            {
+              id: 1,
+              photo: '../src/assets/logo.png',
+              userName: 'Daniel Clifford',
+              userDescribe: 'Verified Graduate',
+              title:'I received a job offer mid-course, and the subjects I learned were current, if not more so, in the company I joined. I honestly feel I got every penny’s worth.',
+              content: '“ I was an EMT for many years before I joined the bootcamp. I’ve been looking to make a transition and have heard some people who had an amazing experience here. I signed up for the free intro course and found it incredibly fun! I enrolled shortly thereafter. The next 12 weeks was the  best - and most grueling - time of my life. Since completing the course, I’ve  successfully switched careers, working as a Software Engineer at a VR startup. ”'
+            }
+          ]
+        };
+    },
+}
 </script>
---
+++ src/components/Card.vue
@@ -0,0 +1,16 @@
+<template>
+    <div :class="[`md:w-20 w-4/5 bg-${bgColor} rounded-lg`]">
+        <img :src="cardContent.photo">
+    </div>
+</template>
+<script>
+export default {
+    name: 'card',
+    props:['bgColor', 'cardContent'],
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
